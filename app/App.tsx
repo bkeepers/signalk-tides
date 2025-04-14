0ae64ca66a64fc3dbba06e68f161178e5825ad5c
Brandon Keepers <brandon@opensoul.org>
2025-04-13 20:37:54 -0400

    Clean up styles, add byline

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import './App.css'
 import { TidesView } from './views/TidesView'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
     <StrictMode>
       <TidesView />
+      <Footer />
     </StrictMode>
   )
 }
---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer>
+      Made by
+      <a href="https://github.com/sponsors/bkeepers"> @bkeepers</a>
+    </footer>
+  )
+}
