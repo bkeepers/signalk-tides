a047cc2cae8f8fb33dd9f3691e2ec02d8c79b626
Brandon Keepers <brandon@opensoul.org>
2025-04-13 21:56:58 -0400

    Add Tailwind

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
-    <footer>
+    <footer className="px-6 py-4 text-center text-sm text-white/50 bg-black/20">
       Made by
-      <a href="https://github.com/sponsors/bkeepers"> @bkeepers</a>
+      <a className="decoration-none text-white/70" href="https://github.com/sponsors/bkeepers"> @bkeepers</a>
     </footer>
   )
 }
--- vite.config.mts
+++ vite.config.mts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,8 @@ export default defineConfig({
   build: {
     outDir: 'public',
   },
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
 })
