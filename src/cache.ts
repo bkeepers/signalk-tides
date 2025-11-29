88d43c264bc16a6f5d8cd7d91a4e63ab24074e45
Brandon Keepers <brandon@opensoul.org>
2025-11-29 07:37:23 -0500

    Fix lint errors

--- src/cache.ts
+++ src/cache.ts
@@ -2,19 +2,18 @@ import { join } from "path";
 import { writeFile, readFile, mkdir } from "fs/promises";
 
 export default class FileCache {
-  constructor(public readonly path: string) {
-  }
+  constructor(public readonly path: string) {}
 
-  async get(key: string): Promise<any | undefined> {
+  async get(key: string): Promise<unknown | undefined> {
     try {
-      const content = await readFile(this.getKey(key), 'utf-8');
+      const content = await readFile(this.getKey(key), "utf-8");
       return JSON.parse(content);
-    } catch (err) {
+    } catch {
       return undefined;
     }
   }
 
-  async set(key: string, value: any) {
+  async set(key: string, value: unknown) {
     // Ensure directory exists
     await mkdir(this.path, { recursive: true });
     return writeFile(this.getKey(key), JSON.stringify(value));
